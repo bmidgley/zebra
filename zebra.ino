e4821d9642b52b5f277e18748a23bc466e19f047
Brad Midgley <bmidgley@gmail.com>
2019-06-07 04:37:19 +0000

    whitespace

--- zebra.ino
+++ zebra.ino
@@ -11,31 +11,11 @@ CRGB leds[NUM_LEDS];
 
 #define UPDATES_PER_SECOND 100
 
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
-
-
-
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
 
 extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
 
 void setup() {
     delay( 3000 ); // power-up safety delay
@@ -47,7 +27,6 @@ void setup() {
     
     pinMode(piezoPin, INPUT);
 }
-
 
 void loop()
 {
